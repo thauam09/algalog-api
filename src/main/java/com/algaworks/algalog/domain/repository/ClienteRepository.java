01146f143c29c5fdbe87e73dd36fd2dbfd206575
Thauã Martins <thauam_09martins@hotmail.com>
2022-09-17 12:12:18 -0300

    usando Spring Data JPA para criar um repositorio

---
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,19 @@
+package com.algaworks.algalog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	// Spring Data JPA provides generation of query methods automatically by
+	// providing to the method name sufix + "by" + property name.
+	// Ex. find + by + name = findByName
+
+	List<Cliente> findByNome(String nome);
+
+	List<Cliente> findByNomeContaining(String nome);
+}
